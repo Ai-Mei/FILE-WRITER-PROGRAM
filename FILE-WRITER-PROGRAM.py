c433940854ece7021d3f645d71b34eef4a837396
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-23 18:59:43 +0800

    The procedure method is created.

--- FILE-WRITER-PROGRAM.py
+++ FILE-WRITER-PROGRAM.py
@@ -1,6 +1,13 @@
+def write_input():
 # Open myfile.txt (append).
+    with open("myfile.txt", "a") as output_file:
 # Ask for user's input and store it as input_line.
+        user_input = input("Enter line: ")
 # Write the user's input to myfile.txt.
+        output_file.write(user_input + "\n")
+
+write_input()
+
 # Ask the user if they have more lines to enter.
 # If yes, repeat the procedure.
 # If no, the program is done.
