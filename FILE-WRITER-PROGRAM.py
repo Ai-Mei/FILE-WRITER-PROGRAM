cdfe69048499c8c72c8b6633259742e584c88adc
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-23 18:34:27 +0800

    Pseudocode

--- FILE-WRITER-PROGRAM.py
+++ FILE-WRITER-PROGRAM.py
@@ -1,1 +1,6 @@
-
+# Open myfile.txt (append).
+# Ask for user's input and store it as input_line.
+# Write the user's input to myfile.txt.
+# Ask the user if they have more lines to enter.
+# If yes, repeat the procedure.
+# If no, the program is done.
